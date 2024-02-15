c5dc42ab94ebea02664cdfb3e32676f83ec3c9b8
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-02-15 17:02:23 +0100

    ajout de nouvelles unités (langues, descriptions, paramétrage)

--- src-Delphi/SutomHelper.dpr
+++ src-Delphi/SutomHelper.dpr
@@ -7,7 +7,12 @@ uses
   u_urlOpen in 'lib-externes\librairies\u_urlOpen.pas',
   Olf.FMX.AboutDialog in 'lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
   Olf.FMX.AboutDialogForm in 'lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
-  uDMLogo in 'uDMLogo.pas' {dmLogo: TDataModule};
+  uDMLogo in 'uDMLogo.pas' {dmLogo: TDataModule},
+  uAboutBoxDescriptionTextConst in 'uAboutBoxDescriptionTextConst.pas',
+  uAboutBoxLicenseTextConst in 'uAboutBoxLicenseTextConst.pas',
+  Olf.RTL.Language in 'lib-externes\librairies\Olf.RTL.Language.pas',
+  Olf.RTL.Params in 'lib-externes\librairies\Olf.RTL.Params.pas',
+  uConfig in 'uConfig.pas';
 
 {$R *.res}
 
