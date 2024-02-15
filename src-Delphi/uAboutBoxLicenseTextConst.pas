c0193c074d26441abf54985636466be1c9086c68
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-02-15 17:00:52 +0100

    ajout d'un texte pour la licence licence affichée dans la boite de dialogue "à propos" (#3)

---
+++ src-Delphi/uAboutBoxLicenseTextConst.pas
@@ -0,0 +1,30 @@
+unit uAboutBoxLicenseTextConst;
+
+interface
+
+const
+  CAboutBoxLicenseFR =
+    'Ce programme est distribué en shareware. Si vous l''utilisez (surtout ' +
+    'dans le cadre d''une activité commerciale ou rémunératrice), merci de ne '
+    + 'pas oublier l''auteur et de contribuer à son développement par l''achat '
+    + 'd''une licence.' + slinebreak + slinebreak +
+    'Ce logiciel est fourni tel quel, avec peut-être des bogues ou sans. Aucune '
+    + 'garantie sur son fonctionnement ni sur les données traitées n''est proposée. '
+    + 'Faites des backups !';
+
+  CAboutBoxLicenseEN =
+    'This program is distributed as shareware. If you use it (especially for ' +
+    'commercial or income-generating purposes), please remember the author and '
+    + 'contribute to its development by purchasing a license.' + slinebreak +
+    slinebreak +
+    'This software is supplied as is, with or without bugs. No warranty is offered '
+    + 'as to its operation or the data processed. Make backups!';
+
+  CAboutBoxLicenseDE = CAboutBoxLicenseEN; // TODO : traduire en allemand
+  CAboutBoxLicenseIT = CAboutBoxLicenseEN; // TODO : traduire en italien
+  CAboutBoxLicensePT = CAboutBoxLicenseEN; // TODO : traduire en portugais
+  CAboutBoxLicenseSP = CAboutBoxLicenseEN; // TODO : traduire en espagnol
+
+implementation
+
+end.
