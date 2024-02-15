64b8affb771214d0ebd81a087d65179c81b0131d
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-02-15 17:00:37 +0100

    ajout d'un texte de description affiché dans la boite de dialogue "A propos" (#2)

---
+++ src-Delphi/uAboutBoxDescriptionTextConst.pas
@@ -0,0 +1,39 @@
+unit uAboutBoxDescriptionTextConst;
+
+interface
+
+const
+  CAboutBoxDescriptionFR =
+    'SUTOM Helper est vous aide à résoudre les challenges de mots '
+    + 'à trouver comme SUTOM (https://sutom.nocle.fr), WORDLE (https://wordle.louan.me), '
+    + 'TUSMO (https://www.tusmo.xyz/), des mots croisés ou fléchés et les autres '
+    + 'quelle que soit la langue utilisée.' + slinebreak + '' + slinebreak +
+    'Choisissez le nombre de lettres, indiquez pour chaque position la lettre à '
+    + 'utiliser puis la liste des lettres possibles afin d''obtenir la liste des '
+    + 'combinaisons possibles. A vous de faire le choix parmi elles en cultivant '
+    + 'votre vocabulaire.' + slinebreak + '' + slinebreak +
+    'Et profitez en pour ouvrir un dictionnaire et apprendre les mots que vous '
+    + 'ne connaissiez pas.';
+
+  CAboutBoxDescriptionEN =
+    'SUTOM Helper help you to solve word-finding challenges such as SUTOM '
+    + '(https://sutom.nocle.fr), WORDLE (https://wordle.louan.me), TUSMO ' +
+    '(https://www.tusmo.xyz/), crosswords or arrow puzzles and others in any language.'
+    + slinebreak + '' + slinebreak +
+    'Choose the number of letters, indicate for each position the letter to be '
+    + 'used and then the list of possible letters to obtain the list of possible '
+    + 'combinations. It''s up to you to choose from among them as you build up '
+    + 'your vocabulary.' + slinebreak + '' + slinebreak +
+    'And take the opportunity to open a dictionary and learn the words you didn''t know.';
+
+  CAboutBoxDescriptionDE = CAboutBoxDescriptionEN;
+  // TODO : traduire en allemand
+  CAboutBoxDescriptionIT = CAboutBoxDescriptionEN; // TODO : traduire en italien
+  CAboutBoxDescriptionPT = CAboutBoxDescriptionEN;
+  // TODO : traduire en portugais
+  CAboutBoxDescriptionSP = CAboutBoxDescriptionEN;
+  // TODO : traduire en espagnol
+
+implementation
+
+end.
