d5d9d4aa192983610d2252fcf44e7513dff2b350
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-18 10:36:53 +0200

    added copyright and timestamp comments

--- src-Delphi/fMain.pas
+++ src-Delphi/fMain.pas
@@ -1,3 +1,35 @@
+(* C2PP
+  ***************************************************************************
+
+  SUTOM Helper
+
+  Copyright 2023-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://sutomhelper.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/SUTOM_Helper
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:43:04.820+02:00
+  Signature : 60f37a7b93b4a7168379d6b14091377c12e206e0
+  ***************************************************************************
+*)
+
 unit fMain;
 
 interface
--- src-Delphi/uDMLogo.pas
+++ src-Delphi/uDMLogo.pas
@@ -1,3 +1,35 @@
+(* C2PP
+  ***************************************************************************
+
+  SUTOM Helper
+
+  Copyright 2023-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://sutomhelper.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/SUTOM_Helper
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:43:04.820+02:00
+  Signature : 20504f128f23090648da54cf7caaf3d58f43577c
+  ***************************************************************************
+*)
+
 unit uDMLogo;
 
 interface
